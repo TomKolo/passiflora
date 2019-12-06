161ad09929a5043ec2d9c7e3d0b7d276234390d9
TomKolo <tomasz.kolodziej.tk@gmail.com>
2019-12-06 13:24:48 +0100

    Adding setup files
    setup files, some changes in test scripts may be required

---
+++ setup.sh
@@ -0,0 +1,7 @@
+mkdir data
+cd data
+curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+gunzip t*-ubyte.gz
