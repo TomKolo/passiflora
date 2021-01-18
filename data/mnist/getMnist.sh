f0955e7a38bfbf8f9dc055903b5fc103b0caadf4
TomKolo <tomasz.kolodziej.tk@gmail.com>
2021-01-18 23:29:20 +0100

    Reorganizing TFF structure

---
+++ data/mnist/getMnist.sh
@@ -0,0 +1,7 @@
+cd data/mnist
+curl -O http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz
+curl -O http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz
+gunzip t*-ubyte.gz
+
